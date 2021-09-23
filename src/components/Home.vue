07416064a1d10eca95c09f54352e7ebcc4dad5eb
litao <litao@stargraph.cn>
2021-09-23 20:27:09 +0800

    y轴设置

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,86 +1,189 @@
 <template>
-  <div id="echartDom">
+  <div id="wrap">
+    <div class="left">
+      <div id="echartDom"></div>
+      <div id="echartDom1"></div>
+    </div>
+    <div class="right">
+      <ul>
+        <li></li>
+      </ul>
+    </div>
   </div>
 </template>
 
 <script>
+import axios from "axios";
+import * as echarts from "echarts";
 
-import axios from 'axios'
-import * as echarts from 'echarts';
-
+import timeInfo from './timestamp.js'
 export default {
-  name: 'Home',
+  name: "Home",
+  data() {
+    return {
+      // baseInfo: [{
+      //     name: '换手率'
+      // },
+      // {
+      //     name: '昨收'
+      // }],
+      timeInfo: timeInfo,
+      avg_price: [],
+      current: [],
+      volume: [],
+    };
+  },
   mounted() {
-    axios.get('http://d.10jqka.com.cn/v6/time/hs_002202/defer/last.js').then(res => {
-      console.log(res)
-    })
-    this.originData()
+    this.getInfo()
+    // setInterval( () => {
+      this.getInfo()
+    // }, 5000);
   },
   methods: {
-    originData() {
-      var chartDom = document.getElementById('echartDom');
-      var myChart = echarts.init(chartDom);
-      var option;
+    getInfo() {
+      axios.get("http://localhost:3000/users").then((res) => {
+        this.avg_price = [];
+        this.current = [];
+        this.volume = [];
 
-      function getVirtulData(year) {
-          year = year || '2017';
-          var date = +echarts.number.parseDate(year + '-01-01');
-          var end = +echarts.number.parseDate((+year + 1) + '-01-01');
-          var dayTime = 3600 * 24 * 1000;
-          var data = [];
-          for (var time = date; time < end; time += dayTime) {
-              data.push([
-                  echarts.format.formatTime('yyyy-MM-dd', time),
-                  Math.floor(Math.random() * 10000)
-              ]);
-          }
-          return data;
-      }
-
-      option = {
-          title: {
-              top: 30,
-              left: 'center',
-              text: '2016年某人每天的步数'
-          },
-          tooltip: {},
-          visualMap: {
-              min: 0,
-              max: 10000,
-              type: 'piecewise',
-              orient: 'horizontal',
-              left: 'center',
-              top: 65
-          },
-          calendar: {
-              top: 120,
-              left: 30,
-              right: 30,
-              cellSize: ['auto', 13],
-              range: '2016',
+        var originInfo = [];
+        var recordCurrent = "";
+        if (res.status == 200 && res.data && res.data.data) {
+          originInfo = res.data.data;
+        }
+        originInfo.items.forEach((item, index) => {
+          this.current.push(item.current); // 股价
+          this.avg_price.push(item.avg_price); // 平均价
+          
+          // 成交量
+          recordCurrent =
+            index !== 0
+              ? originInfo.items[index - 1].current
+              : originInfo.items[0].current;
+          if (index === 0 || item.current < recordCurrent) {
+            this.volume.push({
+              value: item.volume,
               itemStyle: {
-                  borderWidth: 0.5
+                color: "green",
               },
-              yearLabel: {show: false}
-          },
-          series: {
-              type: 'heatmap',
-              coordinateSystem: 'calendar',
-              data: getVirtulData(2016)
+            });
+          } else {
+            this.volume.push({
+              value: item.volume,
+              itemStyle: {
+                color: "red",
+              },
+            });
           }
+
+          // 基础信息
+          // this,baseInfo = {
+          //     name: ,
+          //     value: ,
+          // }
+        });
+        this.originData();
+      });
+    },
+    originData() {
+      var chartDom = document.getElementById("echartDom");
+      var chartDom1 = document.getElementById("echartDom1");
+      var myChart = echarts.init(chartDom);
+      var myChart1 = echarts.init(chartDom1);
+      var option = {
+        xAxis: [
+          {
+            type: "category",
+            data: this.timeInfo,
+          },
+        ],
+        yAxis: [
+          {
+            type: "value",
+            scale: true,
+              position: 'left',
+              axisLine: {
+                show: true,
+              } 
+          },
+          {
+            type: "value",
+            scale: true,
+             position: 'right',
+             axisLine: {
+                show: true,
+              }
+          }
+        ],
+        series: [
+          {
+            data: this.current,
+            type: "line",
+            smooth: true,
+            symbol: "none",
+          },
+          {
+            data: this.avg_price,
+            type: "line",
+            smooth: true,
+            symbol: "none",
+            
+          },
+          {
+            data: [0,1,2,3,4,5,6,7,8,9],
+            type: "line",
+            smooth: true,
+            symbol: "none",
+             yAxisIndex : 1,
+          },
+        ],
+      };
+
+      var option1 = {
+        // xAxis: [
+        //   {
+        //     type: "category",
+        //     data: this.timeInfo,
+        //   },
+        // ],
+        // yAxis: [
+        //   {
+        //     type: "value",
+        //   },
+        // ],
+        // series: [
+        //   {
+        //     data: this.volume,
+        //     type: "bar",
+        //   },
+        // ],
       };
 
       option && myChart.setOption(option);
-
-    }
-  }
-}
+      option1 && myChart1.setOption(option1);
+    },
+  },
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+#wrap {
+  width: 100%;
+  height: 600px;
+}
 #echartDom {
   width: 100%;
   height: 400px;
+}
+#echartDom1 {
+  width: 100%;
+  height: 200px;
+}
+.left {
+  width: 50%;
+}
+.right {
+  width: 50%;
 }
 </style>
